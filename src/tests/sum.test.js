ab43aad311593dec9dad460dd141682bd25c55a1
santiago1023 <santiago.bedoyad@udea.edu.co>
2023-05-09 19:22:49 -0500

    se agregó otra prueba sencilla para ensayar pipeline

--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -8,3 +8,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds -3 + 2 to equal -1', () => {
     expect(sum(-3,2)).toBe(-1);
 })
+
+test('adds 7 + 10 to equal 17', () => {
+    expect(sum(7,10)).toBe(17);
+})
