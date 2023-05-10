b95e5148ad797c710a05f642c7440b19d201c27b
santiago1023 <santiago.bedoyad@udea.edu.co>
2023-05-09 19:10:51 -0500

    tengo dos pruebas con jest

--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -4,3 +4,7 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
 })
+
+test('adds -3 + 2 to equal -1', () => {
+    expect(sum(-3,2)).toBe(-1);
+})
