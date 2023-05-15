18fdc0210ff1b62db951cb7445c9e8300f3fbbed
Daniel <daniel.lujan@udea.edu.co>
2023-05-15 17:08:53 -0500

    Authentication Context

--- src/App.js
+++ src/App.js
@@ -16,25 +16,35 @@ import {
   INFORM_ROUTE,
   LOGIN_ROUTE,
 } from "./middleware/constants";
+import AuthContext from "./components/contexts/AuthContext";
+import { useState } from "react";
 
 function App() {
   const queryClient = new QueryClient();
 
+  const [authUser, setAuthUser] = useState({
+    userId: null,
+    role: null,
+    email: null,
+  });
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<div>Inicio</div>} />
-          <Route path={LOGIN_ROUTE} element={<Login />} />
-          <Route path={COURSE_SELECT_ROUTE} element={<SelectCourse />} />
-          <Route path={FORM_ROUTE} element={<Form />} />
-          <Route path={CONSULT_EVALUATION_ROUTE} element={<Consult />} />
-          <Route path={CONSULT_RESULTS_ROUTE} element={<Results />} />
-          <Route path={INFORM_ROUTE} element={<Inform />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <AuthContext.Provider value={{ ...authUser, setData: setAuthUser }}>
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div>Inicio</div>} />
+            <Route path={LOGIN_ROUTE} element={<Login />} />
+            <Route path={COURSE_SELECT_ROUTE} element={<SelectCourse />} />
+            <Route path={FORM_ROUTE} element={<Form />} />
+            <Route path={CONSULT_EVALUATION_ROUTE} element={<Consult />} />
+            <Route path={CONSULT_RESULTS_ROUTE} element={<Results />} />
+            <Route path={INFORM_ROUTE} element={<Inform />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </AuthContext.Provider>
   );
 }
 
