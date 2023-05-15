75e49e29e2eacc94b00173d727bb1a7d72b9d05b
Daniel <daniel.lujan@udea.edu.co>
2023-05-15 17:17:07 -0500

    Added partial form request

--- src/middleware/fetchers.js
+++ src/middleware/fetchers.js
@@ -29,17 +29,44 @@ export async function login({ email, password }) {
   }
 }
 
-export async function sendForm({ formData, id }) {
-  const res = await fetch(`${API_URL}/form`, {
+async function sendPartialForm({ formData, id, endpointComplement }) {
+  const res = await fetch(`${API_URL}/${endpointComplement}/form`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ ...formData, id }),
-    credentials: "include",
   });
 
   if (!res.ok) {
     throwError(res);
   }
+}
+
+export async function sendForm({ formData, id }) {
+  await sendPartialForm({
+    id,
+    formData: {
+      q1: formData.q1,
+      q2: formData.q2,
+      q3: formData.q3,
+      feedback: formData.feedback,
+    },
+    endpointComplement: "course",
+  });
+
+  await sendPartialForm({
+    id,
+    formData: {
+      q1: formData.q4,
+      q2: formData.q5,
+      q3: formData.q6,
+      q4: formData.q7,
+      q5: formData.q8,
+      q6: formData.q9,
+      q7: formData.q10,
+      feedback: formData.feedback,
+    },
+    endpointComplement: "teacher",
+  });
 }
