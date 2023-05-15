a4c903de90f2b87284215589ae6dbd579eabc29b
Daniel <daniel.lujan@udea.edu.co>
2023-05-15 17:13:01 -0500

    Auth Context and navigate state added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,38 @@
-import React, { useRef } from "react";
+import React, { useContext, useRef } from "react";
 import { useMutation } from "react-query";
 import { useNavigate } from "react-router";
 import { Content } from "../components/Content";
 import * as fetchers from "../middleware/fetchers";
-import { CONSULT_EVALUATION_ROUTE, FORM_ROUTE } from "../middleware/constants";
+import {
+  CONSULT_EVALUATION_ROUTE,
+  COURSE_SELECT_ROUTE,
+} from "../middleware/constants";
+import AuthContext from "../components/contexts/AuthContext";
 
 export const Login = () => {
+  function handleLoginSuccess(data) {
+    auth.setData({
+      email: emailRef.current.value,
+      role: data.role,
+      userId: data.userId,
+    });
+    if (data.role === "student") {
+      navigate(COURSE_SELECT_ROUTE, { state: data.courses });
+    } else {
+      navigate(CONSULT_EVALUATION_ROUTE);
+    }
+  }
+
   const loginMutation = useMutation({
     mutationFn: fetchers.login,
-    onSuccess: (data) => {
-      if (data.role === "student") {
-        navigate(FORM_ROUTE);
-      } else {
-        navigate(CONSULT_EVALUATION_ROUTE);
-      }
-    },
+    onSuccess: handleLoginSuccess,
   });
 
   const emailRef = useRef();
   const passwordRef = useRef();
   const navigate = useNavigate();
+
+  const auth = useContext(AuthContext);
 
   return (
     <Content>
