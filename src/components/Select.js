2ae8c11c77b5be9b850f4d9d8df26aff268c283d
Daniel <daniel.lujan@udea.edu.co>
2023-05-19 09:50:32 -0500

    Select component refactored

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { cloneElement, useState } from "react";
+import Icon from "./icons/Icon";
+import "./Select.css";
+import { AnimatePresence, motion } from "framer-motion";
+import { FADE_DROPDOWN } from "./animations/framer-animations";
 
-const Select = ({ options, onChange }) => {
+export const SelectOption = ({ text, value, onClick }) => {
   return (
-    <select onChange={onChange}>
-      <option value="-1">Seleccione una opción</option>
-      {options.map((item, i) => (
-        <option key={i} value={item.value}>
-          {item.label}
-        </option>
-      ))}
-    </select>
+    <div
+      className="dropdown-select-option"
+      onClick={() => onClick(text, value)}
+    >
+      <p>{text}</p>
+    </div>
+  );
+};
+
+const Select = ({ children, setter, defaultText, style }) => {
+  function handleChange(text, value) {
+    setSelected(text);
+    setter(value);
+    setOpen(false);
+  }
+
+  const [open, setOpen] = useState(null);
+  const [selected, setSelected] = useState(defaultText ?? "");
+
+  return (
+    <div className="dropdown-select" style={style}>
+      <p>{selected}</p>
+      <Icon
+        className="dropdown-select-icon"
+        icon="selector"
+        onClick={() => setOpen(!open)}
+      />
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            className="dropdown-options-container"
+            variants={FADE_DROPDOWN}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+          >
+            {Array.isArray(children)
+              ? children.map((item) =>
+                  cloneElement(item, { onClick: handleChange })
+                )
+              : typeof children !== "undefined" &&
+                cloneElement(children, { onClick: handleChange })}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
 
