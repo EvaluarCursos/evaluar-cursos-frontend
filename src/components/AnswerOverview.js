28ca939df08909ebcfaafc0aea97776f7826bb25
Daniel <daniel.lujan@udea.edu.co>
2023-05-19 09:48:07 -0500

    Send Button added

--- src/components/AnswerOverview.js
+++ src/components/AnswerOverview.js
@@ -11,7 +11,13 @@ const QUALIFICATIONS = {
   5: "Excelente",
 };
 
-const AnswerOverview = ({ answers, onEditAnswer }) => {
+const AnswerOverview = ({
+  answers,
+  onEditAnswer,
+  onReturn,
+  onSend,
+  disableButton,
+}) => {
   function handleEditClick(id) {
     try {
       onEditAnswer(id);
@@ -20,32 +26,51 @@ const AnswerOverview = ({ answers, onEditAnswer }) => {
 
   return (
     <Content>
-      <div className="container">
-        <p className="title">TUS RESPUESTAS</p>
-        <div className="ans-overview">
-          {Object.keys(QUESTIONS).map(
-            (q) =>
-              q !== "feedback" && (
-                <div key={q} className="ans-overview-row">
-                  <p>{QUESTIONS[q].question}</p>
-                  <div className="qualification">
-                    <p>
-                      {answers[q] ? (
-                        QUALIFICATIONS[answers[q]]
-                      ) : (
-                        <Icon icon="forbid" size={18} />
-                      )}
-                    </p>
-                    <Icon
-                      className="edit-button"
-                      icon="editCircle"
-                      size={18}
-                      onClick={() => handleEditClick(q)}
-                    />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          marginBottom: "40px",
+        }}
+      >
+        <div className="container">
+          <p className="title">TUS RESPUESTAS</p>
+          <div className="ans-overview">
+            {Object.keys(QUESTIONS).map(
+              (q) =>
+                q !== "feedback" && (
+                  <div key={q} className="ans-overview-row">
+                    <p>{QUESTIONS[q].question}</p>
+                    <div className="qualification">
+                      <p>
+                        {answers[q] ? (
+                          QUALIFICATIONS[answers[q]]
+                        ) : (
+                          <Icon icon="forbid" size={18} />
+                        )}
+                      </p>
+                      <Icon
+                        className="edit-button"
+                        icon="editCircle"
+                        size={18}
+                        onClick={() => handleEditClick(q)}
+                      />
+                    </div>
                   </div>
-                </div>
-              )
-          )}
+                )
+            )}
+          </div>
+        </div>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <button className="form" onClick={onReturn}>
+            <Icon icon="arrowBack" style={{ marginRight: "6px" }} />
+            Volver
+          </button>
+          <button className="form" onClick={onSend} disabled={disableButton}>
+            {disableButton ? "Enviando..." : "Enviar"}
+            <Icon icon="send" style={{ marginLeft: "6px" }} />
+          </button>
         </div>
       </div>
     </Content>
