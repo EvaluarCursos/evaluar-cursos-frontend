8935db6e3156f22a12c43e0104b4741dba51d9df
Daniel <daniel.lujan@udea.edu.co>
2023-05-19 09:47:18 -0500

    Query settings & Notifications

--- src/App.js
+++ src/App.js
@@ -17,10 +17,28 @@ import {
   LOGIN_ROUTE,
 } from "./middleware/constants";
 import AuthContext from "./components/contexts/AuthContext";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import Notification from "./components/Notification";
+import NotificationContext from "./components/contexts/NotificationContext";
 
 function App() {
-  const queryClient = new QueryClient();
+  function notifySuccess(message) {
+    successNotification.current.show(message);
+  }
+
+  function notifyError(message) {
+    errorNotification.current.show(message);
+  }
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false,
+        staleTime: 1000 * 60,
+      },
+    },
+  });
 
   const [authUser, setAuthUser] = useState({
     userId: null,
@@ -28,22 +46,31 @@ function App() {
     email: null,
   });
 
+  const successNotification = useRef();
+  const errorNotification = useRef();
+
   return (
     <AuthContext.Provider value={{ ...authUser, setData: setAuthUser }}>
-      <QueryClientProvider client={queryClient}>
-        <Navbar />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<div>Inicio</div>} />
-            <Route path={LOGIN_ROUTE} element={<Login />} />
-            <Route path={COURSE_SELECT_ROUTE} element={<SelectCourse />} />
-            <Route path={FORM_ROUTE} element={<Form />} />
-            <Route path={CONSULT_EVALUATION_ROUTE} element={<Consult />} />
-            <Route path={CONSULT_RESULTS_ROUTE} element={<Results />} />
-            <Route path={INFORM_ROUTE} element={<Inform />} />
-          </Routes>
-        </BrowserRouter>
-      </QueryClientProvider>
+      <NotificationContext.Provider
+        value={{ success: notifySuccess, error: notifyError }}
+      >
+        <QueryClientProvider client={queryClient}>
+          <Navbar />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<div>Inicio</div>} />
+              <Route path={LOGIN_ROUTE} element={<Login />} />
+              <Route path={COURSE_SELECT_ROUTE} element={<SelectCourse />} />
+              <Route path={FORM_ROUTE} element={<Form />} />
+              <Route path={CONSULT_EVALUATION_ROUTE} element={<Consult />} />
+              <Route path={CONSULT_RESULTS_ROUTE} element={<Results />} />
+              <Route path={INFORM_ROUTE} element={<Inform />} />
+            </Routes>
+          </BrowserRouter>
+          <Notification ref={errorNotification} type="error" />
+          <Notification ref={successNotification} type="success" />
+        </QueryClientProvider>
+      </NotificationContext.Provider>
     </AuthContext.Provider>
   );
 }
